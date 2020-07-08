7b55d64a22a8edc65ae021af8db71aef57c0dd84
ShunsukeKouno111 <kouno.shunsuke@isid.co.jp>
2020-07-08 15:19:29 +0900

    過去の追加

---
+++ Extract_Num.ps1
@@ -0,0 +1,7 @@
+﻿$filepass = "D:\work\wiki_contents_id.log"
+foreach ($line in Get-Content $filepass) {  
+    if($line -match "[0-9]+"){
+        $tmp = $line.Trim("|")
+        $tmp.Trim() | Out-File  D:\work\wiki_contents_id_only_num_bcp.txt -Append -encoding UTF8
+    }
+}
