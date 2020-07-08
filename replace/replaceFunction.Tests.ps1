3c5c7d88539c2395cf687986f4dce605a4755fda
ShunsukeKouno111 <kouno.shunsuke@isid.co.jp>
2020-07-08 19:36:00 +0900

    replaceのテスト追加

--- replace/replaceFunction.Tests.ps1
+++ replace/replaceFunction.Tests.ps1
@@ -2,20 +2,65 @@
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
-Describe "ケース1" {
-    It "正しく変換できるか" {
+Describe "Update-DescriptionSourceLink" {
+    It "ケース1" {
         $SVNSourceLink = "`"source:trunk/src/net/Script/Net-Build.ps1`""
-        $GitHubSourceLink = [regex]::Replace($SVNSourceLink, "`"source:trunk/[^@]+`"", { Update-SourceLink($args[0].Groups[0].Value) })
+        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
         $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/blob/master/src/net/Script/Net-Build.ps1"
     }
-    It "ケース2のURLの変換を行わないか" {
+    It "ケース2" {
         $SVNSourceLink = "`"source:trunk/src/net/Script/Net-Build.ps1@149657`""
-        $GitHubSourceLink = [regex]::Replace($SVNSourceLink, "`"source:trunk/[^@]+`"", { Update-SourceLink($args[0].Groups[0].Value) })
-        $GitHubSourceLink | Should Be $SVNSourceLink
+        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/56d5a47b2a461a34676326f13966652901ad94b5#diff-43758f965282ae160c9890a763c621e2"
     }
-    It "ケース3のURLの変換を行わないか" {
+    It "ケース3" {
         $SVNSourceLink = "`"source:trunk/src/net/Script/Net-Build.ps1@149657#L132`""
-        $GitHubSourceLink = [regex]::Replace($SVNSourceLink, "`"source:trunk/[^@]+`"", { Update-SourceLink($args[0].Groups[0].Value) })
-        $GitHubSourceLink | Should Be $SVNSourceLink
+        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/56d5a47b2a461a34676326f13966652901ad94b5#diff-43758f965282ae160c9890a763c621e2L132"
+    }
+    It "ケース4" {
+        $SVNSourceLink = "`"source:branches/3.4.3/src/net/Common Source/Shared/AssemblyInfoData.cs`""
+        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/blob/3.4.3/src/net/Common%20Source/Shared/AssemblyInfoData.cs"
+    }
+    It "ケース5" {
+        $SVNSourceLink = "`"source:branches/3.4.3/src/net/Common Source/Shared/AssemblyInfoData.cs@155018`""
+        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/7b7dfdfbe2fdf4af1bbb3c36f543edd3f9a3e3eb#diff-dea7ca04b6ca6cab8d12b77a308b71f8"
+    }
+    It "ケース6" {
+        $SVNSourceLink = "`"source:branches/3.4.3/src/net/Common Source/Shared/AssemblyInfoData.cs@155018#L81`""
+        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/7b7dfdfbe2fdf4af1bbb3c36f543edd3f9a3e3eb#diff-dea7ca04b6ca6cab8d12b77a308b71f8L81"
+    }
+    It "ケース7" {
+        $SVNSourceLink = "`"source:plugin/DOORS/trunk/src/Project/Client/Client.DOORS/PluginInfoData.cs`""
+        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/blob/master/src/Project/Client/Client.DOORS/PluginInfoData.cs"
+    }
+    It "ケース8" {
+        $SVNSourceLink = "`"source:plugin/DOORS/trunk/src/Project/Client/Client.DOORS/PluginInfoData.cs@150084`""
+        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/commit/cd6699cc3832cafa5db31f67b94d1cfded472350#diff-937ede871a50d9d43e79b35cb21376ab"
+    }
+    It "ケース9" {
+        $SVNSourceLink = "`"source:plugin//DOORS/trunk/src/Project/Client/Client.DOORS/PluginInfoData.cs@150084#L15`""
+        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/commit/cd6699cc3832cafa5db31f67b94d1cfded472350#diff-937ede871a50d9d43e79b35cb21376abL15"
+    }
+    It "ケース10" {
+        $SVNSourceLink = "`"source:plugin/DOORS/branches/1.343/src/Project/Client/Client.DOORS/PluginInfoData.cs`""
+        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/blob/1.343/src/Project/Client/Client.DOORS/PluginInfoData.cs"
+    }
+    It "ケース11" {
+        $SVNSourceLink = "`"source:plugin/DOORS/branches/1.343/src/Project/Client/Client.DOORS/PluginInfoData.cs@157464`""
+        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/commit/cd6699cc3832cafa5db31f67b94d1cfded472350#diff-937ede871a50d9d43e79b35cb21376ab"
+    }
+    It "ケース12" {
+        $SVNSourceLink = "`"source:plugin/DOORS/branches/1.343/src/Project/Client/Client.DOORS/PluginInfoData.cs@157464#L15`""
+        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/commit/cd6699cc3832cafa5db31f67b94d1cfded472350#diff-937ede871a50d9d43e79b35cb21376abL15"
     }
 }
--- replace/replaceFunction.ps1
+++ replace/replaceFunction.ps1
@@ -7,3 +7,15 @@
     $githubLink = $githubLink.Substring(0, $githubLink.Length - 1)
     return $githubLink
 }
+
+function Update-DescriptionSourceLink {
+    Param(
+        [String]
+        $Description
+    )
+    $updatedDescription = [regex]::Replace($Description, "`"source:.+?`"", { Update-SourceLink($args[0].Groups[0].Value) })
+    return $updatedDescription
+}
+
+$SVNSourceLink = "`"source:trunk/src/net/Script/Net-Build.ps1`" `"source:trunk/src/net/Script/Net-Build.ps1`" `"source:trunk/src/net/Script/Net-Build.ps1`" "
+$GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
