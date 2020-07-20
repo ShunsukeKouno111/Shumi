2e4eb04e93cde55f736002d92f16bb1bde4e019d
ShunsukeKouno111 <kouno.shunsuke@isid.co.jp>
2020-07-20 18:39:47 +0900

    Functionの結合

--- replace/replaceFunction.Tests.ps1
+++ replace/replaceFunction.Tests.ps1
@@ -4,97 +4,102 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 
 
 Describe "Update-DescriptionSourceLink" {
-    It "ケース1" {
-        $SVNSourceLink = "ccc source:`"trunk/src/net/Script/Net-Build.ps1`" fff"
+    # It "ケース1" {
+    #     $SVNSourceLink = "ccc source:`"trunk/src/net/Script/Net-Build.ps1`" fff"
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "ccc https://github.com/ISID/iQUAVIS/blob/master/src/net/Script/Net-Build.ps1  fff"
+    # }
+    # It "ケース1 -ダブルコーテーションなし-" {
+    #     $SVNSourceLink = "aaa source:trunk/src/net/Script/Net-Build.ps1 ccc"
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "aaa https://github.com/ISID/iQUAVIS/blob/master/src/net/Script/Net-Build.ps1  ccc"
+    # }
+    # It "ケース2" {
+    #     $SVNSourceLink = "source:`"trunk/src/net/Script/Net-Build.ps1@149657`""
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/56d5a47b2a461a34676326f13966652901ad94b5#diff-43758f965282ae160c9890a763c621e2 "
+    # }
+    # It "ケース2 -ダブルコーテーションなし-" {
+    #     $SVNSourceLink = "source:trunk/src/net/Script/Net-Build.ps1@149657 "
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/56d5a47b2a461a34676326f13966652901ad94b5#diff-43758f965282ae160c9890a763c621e2 "
+    # }
+    # It "ケース3" {
+    #     $SVNSourceLink = "source:`"trunk/src/net/Script/Net-Build.ps1@149657#L132`""
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/56d5a47b2a461a34676326f13966652901ad94b5#diff-43758f965282ae160c9890a763c621e2L132 "
+    # }
+    # It "ケース3 -ダブルコーテーションなし-" {
+    #     $SVNSourceLink = "bbb source:trunk/src/net/Script/Net-Build.ps1@149657#L132 aaa"
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "bbb https://github.com/ISID/iQUAVIS/commit/56d5a47b2a461a34676326f13966652901ad94b5#diff-43758f965282ae160c9890a763c621e2L132  aaa"
+    # }
+    # It "ケース4" {
+    #     $SVNSourceLink = "source:`"branches/3.4.3/src/net/Common Source/Shared/AssemblyInfoData.cs`""
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/blob/3.4.3/src/net/Common Source/Shared/AssemblyInfoData.cs "
+    # }
+    # It "ケース5" {
+    #     $SVNSourceLink = "source:`"branches/3.4.3/src/net/Common Source/Shared/AssemblyInfoData.cs@155018`""
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/7b7dfdfbe2fdf4af1bbb3c36f543edd3f9a3e3eb#diff-dea7ca04b6ca6cab8d12b77a308b71f8 "
+    # }
+    # It "ケース6" {
+    #     $SVNSourceLink = "source:`"branches/3.4.3/src/net/Common Source/Shared/AssemblyInfoData.cs@155018#L81`""
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/7b7dfdfbe2fdf4af1bbb3c36f543edd3f9a3e3eb#diff-dea7ca04b6ca6cab8d12b77a308b71f8L81 "
+    # }
+    # It "ケース7" {
+    #     $SVNSourceLink = "source:`"plugin/DOORS/trunk/src/Project/Client/Client.DOORS/PluginInfoData.cs`""
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/blob/master/src/Project/Client/Client.DOORS/PluginInfoData.cs "
+    # }
+    # It "ケース8" {
+    #     $SVNSourceLink = "source:`"plugin/DOORS/trunk/src/Project/Client/Client.DOORS/PluginInfoData.cs@150084`""
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/commit/cd6699cc3832cafa5db31f67b94d1cfded472350#diff-937ede871a50d9d43e79b35cb21376ab "
+    # }
+    # It "ケース9" {
+    #     $SVNSourceLink = "source:`"plugin//DOORS/trunk/src/Project/Client/Client.DOORS/PluginInfoData.cs@150084#L15`""
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/commit/cd6699cc3832cafa5db31f67b94d1cfded472350#diff-937ede871a50d9d43e79b35cb21376abL15 "
+    # }
+    # It "ケース10" {
+    #     $SVNSourceLink = "source:`"plugin/DOORS/branches/1.343/src/Project/Client/Client.DOORS/PluginInfoData.cs`""
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/blob/1.343/src/Project/Client/Client.DOORS/PluginInfoData.cs "
+    # }
+    # It "ケース11" {
+    #     $SVNSourceLink = "source:`"plugin/DOORS/branches/1.343/src/Project/Client/Client.DOORS/PluginInfoData.cs@157464`""
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/commit/b460616f2bdb4819a50981bdf2c713b83fa09c7a#diff-937ede871a50d9d43e79b35cb21376ab "
+    # }
+    # It "ケース12" {
+    #     $SVNSourceLink = "source:`"plugin/DOORS/branches/1.343/src/Project/Client/Client.DOORS/PluginInfoData.cs@157464#L15`""
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/commit/b460616f2bdb4819a50981bdf2c713b83fa09c7a#diff-937ede871a50d9d43e79b35cb21376abL15 "
+    # }
+    # It "ケース13" { #../revisions/172558が対象リビジョン、trunk/src/net/Project/Client/Api/Operation/IOperationApi.csが指すファイル
+    #     $SVNSourceLink = "source:../revisions/172558/diff/trunk/src/net/Project/Client/Api/Operation/IOperationApi.cs "
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/236e12385eac1d65c6281a5dce90d9e1d3616cf0#diff-deb1688ce2dcebeb800ca1e539570989 "
+    # }
+    # It "ケース14" { #../diff/ をRemoveすればいける?
+    #     $SVNSourceLink = "pjm:source:../diff/trunk/src/net/Project/Server/Mail/Mail/Triggers/ITaskAlertBatchMailTrigger.cs@125670 "
+    #     $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
+    #     $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/2a610b370965fb100c035a0c34ea7337e226a40b#diff-e029a624dce3132299a83c8926efea0f "
+    # }
+    It "ケース15" { #../diff/ をRemoveすればいける?
+        $SVNSourceLink = "pjm:source:/trunk/src/net/Project/Server/Mail/Mail/Triggers/ProjectModel.cs "
         $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "ccc https://github.com/ISID/iQUAVIS/blob/master/src/net/Script/Net-Build.ps1  fff"
+        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/blob/master/src/net/Project/Server/Mail/Mail/Triggers/ProjectModel.cs  "
     }
-    It "ケース1 -ダブルコーテーションなし-" {
-        $SVNSourceLink = "aaa source:trunk/src/net/Script/Net-Build.ps1 ccc"
-        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "aaa https://github.com/ISID/iQUAVIS/blob/master/src/net/Script/Net-Build.ps1  ccc"
-    }
-    It "ケース2" {
-        $SVNSourceLink = "source:`"trunk/src/net/Script/Net-Build.ps1@149657`""
-        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/56d5a47b2a461a34676326f13966652901ad94b5#diff-43758f965282ae160c9890a763c621e2 "
-    }
-    It "ケース2 -ダブルコーテーションなし-" {
-        $SVNSourceLink = "source:trunk/src/net/Script/Net-Build.ps1@149657 "
-        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/56d5a47b2a461a34676326f13966652901ad94b5#diff-43758f965282ae160c9890a763c621e2 "
-    }
-    It "ケース3" {
-        $SVNSourceLink = "source:`"trunk/src/net/Script/Net-Build.ps1@149657#L132`""
-        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/56d5a47b2a461a34676326f13966652901ad94b5#diff-43758f965282ae160c9890a763c621e2L132 "
-    }
-    It "ケース3 -ダブルコーテーションなし-" {
-        $SVNSourceLink = "bbb source:trunk/src/net/Script/Net-Build.ps1@149657#L132 aaa"
-        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "bbb https://github.com/ISID/iQUAVIS/commit/56d5a47b2a461a34676326f13966652901ad94b5#diff-43758f965282ae160c9890a763c621e2L132  aaa"
-    }
-    It "ケース4" {
-        $SVNSourceLink = "source:`"branches/3.4.3/src/net/Common Source/Shared/AssemblyInfoData.cs`""
-        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/blob/3.4.3/src/net/Common Source/Shared/AssemblyInfoData.cs "
-    }
-    It "ケース5" {
-        $SVNSourceLink = "source:`"branches/3.4.3/src/net/Common Source/Shared/AssemblyInfoData.cs@155018`""
-        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/7b7dfdfbe2fdf4af1bbb3c36f543edd3f9a3e3eb#diff-dea7ca04b6ca6cab8d12b77a308b71f8 "
-    }
-    It "ケース6" {
-        $SVNSourceLink = "source:`"branches/3.4.3/src/net/Common Source/Shared/AssemblyInfoData.cs@155018#L81`""
-        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/7b7dfdfbe2fdf4af1bbb3c36f543edd3f9a3e3eb#diff-dea7ca04b6ca6cab8d12b77a308b71f8L81 "
-    }
-    It "ケース7" {
-        $SVNSourceLink = "source:`"plugin/DOORS/trunk/src/Project/Client/Client.DOORS/PluginInfoData.cs`""
-        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/blob/master/src/Project/Client/Client.DOORS/PluginInfoData.cs "
-    }
-    It "ケース8" {
-        $SVNSourceLink = "source:`"plugin/DOORS/trunk/src/Project/Client/Client.DOORS/PluginInfoData.cs@150084`""
-        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/commit/cd6699cc3832cafa5db31f67b94d1cfded472350#diff-937ede871a50d9d43e79b35cb21376ab "
-    }
-    It "ケース9" {
-        $SVNSourceLink = "source:`"plugin//DOORS/trunk/src/Project/Client/Client.DOORS/PluginInfoData.cs@150084#L15`""
-        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/commit/cd6699cc3832cafa5db31f67b94d1cfded472350#diff-937ede871a50d9d43e79b35cb21376abL15 "
-    }
-    It "ケース10" {
-        $SVNSourceLink = "source:`"plugin/DOORS/branches/1.343/src/Project/Client/Client.DOORS/PluginInfoData.cs`""
-        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/blob/1.343/src/Project/Client/Client.DOORS/PluginInfoData.cs "
-    }
-    It "ケース11" {
-        $SVNSourceLink = "source:`"plugin/DOORS/branches/1.343/src/Project/Client/Client.DOORS/PluginInfoData.cs@157464`""
-        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/commit/b460616f2bdb4819a50981bdf2c713b83fa09c7a#diff-937ede871a50d9d43e79b35cb21376ab "
-    }
-    It "ケース12" {
-        $SVNSourceLink = "source:`"plugin/DOORS/branches/1.343/src/Project/Client/Client.DOORS/PluginInfoData.cs@157464#L15`""
-        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS-DOORS/commit/b460616f2bdb4819a50981bdf2c713b83fa09c7a#diff-937ede871a50d9d43e79b35cb21376abL15 "
-    }
-    It "ケース13" { #../revisions/172558が対象リビジョン、trunk/src/net/Project/Client/Api/Operation/IOperationApi.csが指すファイル
-        $SVNSourceLink = "source:../revisions/172558/diff/trunk/src/net/Project/Client/Api/Operation/IOperationApi.cs "
-        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/236e12385eac1d65c6281a5dce90d9e1d3616cf0#diff-deb1688ce2dcebeb800ca1e539570989 "
-    }
-    It "ケース14" { #../diff/ をRemoveすればいける?
-        $SVNSourceLink = "pjm:source:../diff/trunk/src/net/Project/Server/Mail/Mail/Triggers/ITaskAlertBatchMailTrigger.cs@125670 "
-        $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
-        $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/2a610b370965fb100c035a0c34ea7337e226a40b#diff-e029a624dce3132299a83c8926efea0f "
-    }
-    It "ケース15" { # リポジトリ名/compare/{コミットハッシュ}...{コミットハッシュ}#diff-{MD5}
+    It "ケース16" { # リポジトリ名/compare/{コミットハッシュ}...{コミットハッシュ}#diff-{MD5}
         $SVNSourceLink = "source:`"../diff/trunk/src/net/Project/Client/Infrastructure/Services/MessageDialog/MessageDialog.cs?rev=172609&rev_to=171813`""
         $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
         $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/compare/f772dd003c3cbdf559919bc19eeee07997c88a8e...935adcc7ec6383136bf3a150ff1cd2188ef20e74#diff-628a73a4c57f3a0c8b105da5661aea84 "
     }
-    It "ケース16" { #14のダブルコーテーション版
+    It "ケース17" { #14のダブルコーテーション版
         $SVNSourceLink = "pjm:source:`"../diff/trunk/src/net/Unit Test/Server/Mail\Test.Mail/Triggers/TaskAlertBatchMailTriggerTest.cs@125670`""
         $GitHubSourceLink = Update-DescriptionSourceLink $SVNSourceLink
         $GitHubSourceLink | Should Be "https://github.com/ISID/iQUAVIS/commit/2a610b370965fb100c035a0c34ea7337e226a40b#diff-b51019663c33eb573399dfa0951b13bd "
--- replace/replaceFunction.ps1
+++ replace/replaceFunction.ps1
@@ -6,7 +6,8 @@ function Update-SourceLink {
         $SourceLink
     )
 
-    $revision_hash = Get-RevisionMappingFile "D:\GitRepository\Shumi\replace\pjm-dev"
+    # テスト用
+    #$revision_hash = Get-RevisionMappingFile "D:\GitRepository\Shumi\replace\pjm-dev"
 
     $getHashRepository = {
         return $revision_hash[$args[0]]
@@ -18,8 +19,8 @@ function Update-SourceLink {
     if ($SourceLink.Contains("../diff/")) {
         $SourceLink = $SourceLink -replace "../diff/", ""
     }
-    if ($SourceLink.Contains("pjm:source")) {
-        $SourceLink = $SourceLink -replace "pjm:", ""
+    if ($SourceLink.Contains("pjm:")) {
+        $SourceLink = $SourceLink -replace "pjm:source:/", "source:"
     }
     if ($SourceLink.Contains("\")) {
         $SourceLink = $SourceLink -replace "\\", "\\"
@@ -46,16 +47,7 @@ function Update-SourceLink {
         $line = $array[4] -replace "`"", ""
         $md5 = Get-MD5Hash $directoryPath
         $hash_repository = & $getHashRepository $revision.Trim()
-        # MEMO : リビジョンをコミットハッシュに変換する際にTrimした文末空白を付け直すため
-        # 例 : "source:trunk~~~@125670 "
-        # if ((-not $line) -and ($SourceLink -notcontains "`"")) {
-        #     $line = " "
-        # }
         $githubLink = "$($hash_repository.Values)/commit/$($hash_repository.keys)#diff-$md5$line"
-        # $githubLink = $SourceLink -replace "source:(`"|)(trunk|branches/.*?|plugin/.*?/(trunk|branches/.*?))/[^@]*?", "$($hash_repository.Values)/"
-        # $githubLink = $githubLink -replace $directoryPath, ""
-        # $githubLink = $githubLink -replace "@\d{1,6}", "commit/$($hash_repository.keys)#diff-$md5"
-        # $githubLink = $githubLink -replace "#L", "L"
 
     }
     elseif ($SourceLink.Contains("?rev=")) {
@@ -68,7 +60,6 @@ function Update-SourceLink {
         $toHash_repository = & $getHashRepository $toRevision.Trim()
         $githubLink = $SourceLink -replace "source:(`"|)(trunk|branches/.*?|plugin/.*?/(trunk|branches/.*?))/[^@]*?", "$($hash_repository.Values)/"
         $githubLink = $githubLink -replace $directoryPath, ""
-        #?rev=172609&rev_to=171813
         $githubLink = $githubLink -replace ".rev=\d{1,6}.rev_to=\d{1,6}", "compare/$($hash_repository.keys)...$($toHash_repository.keys)#diff-$md5"
     }
     #ケース1
@@ -152,3 +143,175 @@ function Update-DescriptionSourceLink {
     $updatedDescription = [regex]::Replace($updatedDescription, "(pjm:|)source:.+?\s", { Update-SourceLink($args[0].Groups[0].Value) })
     return $updatedDescription
 }
+
+function Update-RedmineSourceLink {
+    Param(
+        [String]
+        $CsvRootURL,
+        [String]
+        $SVNRootURL
+    )
+
+    & (Join-Path $PSScriptRoot 'ModuleLoader.ps1')
+    Set-DBInstance "localhost\SQLEXPRESS"
+
+    $errorLogPath = Join-Path $PSScriptRoot "error.log"
+    $timeLog = Join-Path $PSScriptRoot "timestamp.log"
+    $script:count = 0
+
+    "importing mapping file."
+    $csvfiles = Get-ChildItem $CsvRootURL -Recurse | Where-Object { $_.Extension -eq ".csv" }
+    foreach ($csvfile in $csvfiles) {
+        $csv = Import-Csv $csvfile.FullName
+        $parentDirName = Split-Path (Split-Path $csvfile.FullName -Parent) -Leaf
+        $repositoryPath = "https://github.com/ISID/$parentDirName"
+        "importing $parentDirName mapping file."
+        foreach ($csvline in $csv) {
+            $script:count++
+            $revision = $csvline.revision
+            $branch = $csvline.branch
+            $hash_repository = @{ }
+            $hash_repository.Add($csvline.hash, $repositoryPath) | Out-Null
+            if ($revision_hash.ContainsKey($revision)) {
+                if ($branch -eq "refs/svn/root/trunk") {
+                    $revision_hash.Remove($revision) | Out-Null
+                    $revision_hash.Add($revision, $hash_repository) | Out-Null
+                }
+            }
+            elseif ($branch -ne '') {
+                $revision_hash.Add($revision, $hash_repository) | Out-Null
+            }
+            if ($script:count % 10000 -eq 0) {
+                "$script:count revision imported."
+                $date = Get-Date
+                "$script:count revision imported. time:$date" | Out-File $timeLog -Append -encoding UTF8
+            }
+        }
+        "imported $parentDirName mapping file."
+    }
+    "imported mapping file."
+
+    $mySqlDllPath = Join-Path ${env:ProgramFiles(x86)} "MySQL\*Connector Net 8.0*\Assemblies\v4.5.2\MySql.Data.dll"
+    if (-not (Test-Path $mySqlDllPath)) {
+        Invoke-ChocoInstall -PackageName "mysql-connector" -Version 8.0.20
+    }
+    Add-Type -Path $mySqlDllPath
+
+    $DB_NAME = "redmine"
+    $MY_SQL_CONNECTION_STRING = "Server=localhost;Database=$($DB_NAME);Uid=root;Pwd=abcdefgH123" # MySQLのrootアカウントのパスワードで****を更新します。
+    $getReaderValues = {
+        $reader = $args[0]
+        $values = New-Object object[] ($reader.FieldCount)
+        $reader.GetValues($values) | Out-Null
+        return $values
+    }
+
+    $toMySqlQuery = {
+        return $args[0].Replace("$($args[1]).", [string]::Empty).Replace('[', '`').Replace(']', '`')
+    }
+
+    $getMySqlDbCommand = {
+        $result = @{
+            Command = $null;
+            Cleanup = $null;
+        }
+        $mySqlConn = New-Object MySql.Data.MySqlClient.MySqlConnection $MY_SQL_CONNECTION_STRING
+        $mySqlCommand = $mySqlConn.CreateCommand()
+        $mySqlCommand.CommandTimeout = 0
+        $mySqlConn.Open()
+        $result.Command = $mySqlCommand
+        $result.Cleanup = {
+            if ($mySqlCommand) { $mySqlCommand.Dispose() }
+            if ($mySqlConn) { $mySqlConn.Close() }
+        }.GetNewClosure()
+        return $result
+    }
+
+    $sql = "select ISSUES.Id,ISSUES.description,REPO.url,REPO.root_url,ISSUES.project_id
+        from ISSUES
+        inner join repositories REPO
+        on ISSUES.project_id = REPO.project_id
+        where root_url ='$SVNRootURL'
+        order by ISSUES.Id;"
+    $schemaName = "[dbo]"
+    try {
+        $mySqlCommandResult = & $getMySqlDbCommand
+        $command = $mySqlCommandResult.Command
+        $command.CommandText = & $toMySqlQuery $sql $schemaName
+        $script:count = 0
+        $changedIssues = New-Object System.Collections.ArrayList
+        $mySqlReader = $command.ExecuteReader()
+        while ($mySqlReader.Read()) {
+            $mySqlValues = & $getReaderValues $mySqlReader
+            if ($mySqlValues -isnot [array]) {
+                $mySqlValues = @($mySqlValues)
+            }
+            $issue = New-Object PSCustomObject -Property @{id = ""; description = "" }
+            $issue.id = $mySqlValues[0]
+            $originalDescription = $mySqlValues[1]
+            $issue.description = Update-DescriptionSourceLink $originalDescription
+            if ($originalDescription -ne $issue.description) {
+                $changedIssues.Add($issue) | Out-Null
+            }
+            $script:count++
+            if ($script:count % 10000 -eq 0) {
+                "$script:count issues is checked."
+                $date = Get-Date
+                "$script:count issues is checked. time:$date" | Out-File $timeLog -Append -encoding UTF8
+            }
+            trap {
+                Out-File -InputObject $issue.id -FilePath $errorLogPath -Encoding (COnvertTo-EncodingParameter (New-Object Text.UTF8Encoding $true)) -NoNewLine
+                break
+            }
+        }
+    }
+    finally {
+        if ($mySqlCommandResult) {
+            $mySqlCleanup = $mySqlCommandResult.Cleanup
+            if ($mySqlCleanup) {
+                & $mySqlCleanup
+            }
+        }
+    }
+
+    try {
+        $script:count = 0
+        foreach ($issue in $changedIssues) {
+            "#$($issue.id) is updating."
+            $description = [MySql.Data.MySqlClient.MySqlHelper]::EscapeString($issue.description)
+            $mySqlCommandResult = & $getMySqlDbCommand
+            $command = $mySqlCommandResult.Command
+            $updateSql = "UPDATE issues set description = '$description' where id = $($issue.id);"
+            $command.CommandText = $updateSql
+            $command.ExecuteNonQuery() | Out-Null
+            "#$($issue.id) is updated."
+            $script:count++
+            if ($script:count % 10000 -eq 0) {
+                "$script:count issues is updated."
+                $date = Get-Date
+                "$script:count issues is updated. time:$date" | Out-File $timeLog -Append -encoding UTF8
+            }
+            if ($mySqlCommandResult) {
+                $mySqlCleanup = $mySqlCommandResult.Cleanup
+                if ($mySqlCleanup) {
+                    & $mySqlCleanup
+                }
+            }
+            trap {
+                Out-File -InputObject $updateSql -FilePath $errorLogPath -Encoding (COnvertTo-EncodingParameter (New-Object Text.UTF8Encoding $true)) -NoNewLine
+                break
+            }
+        }
+    }
+    finally {
+        if ($mySqlCommandResult) {
+            $mySqlCleanup = $mySqlCommandResult.Cleanup
+            if ($mySqlCleanup) {
+                & $mySqlCleanup
+            }
+        }
+    }
+}
+
+Update-RedmineSourceLink -CsvRootURL "D:\GitRepository\mappingfile\pjm-dev" -SVNRootURL "http://ksvnrp05.isid.co.jp/pjm-dev"
+Update-RedmineSourceLink -CsvRootURL "D:\GitRepository\mappingfile\iquavis-plugin" -SVNRootURL "http://ksvnrp16.isid.co.jp/iquavis-plugin"
